331f683735585055b8a057a3bf4ed5b3749586ba
Arrmen Lee <wachemanstonphoenix@gmail.com>
2020-08-12 17:14:41 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const distPath = path.resolve(__dirname, 'dist');
+
+const config = {
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/env', {
+                                        modules: false,
+                                    },
+                                ],
+                            ],
+                            plugins: [
+                                'transform-object-rest-spread',
+                                '@babel/plugin-transform-react-jsx'
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    devServer: {
+        contentBase: distPath,
+        compress: true,
+        port: 8080
+    }
+};
+
+const main = Object.assign({}, config, {
+    entry: {
+        index: './src/index.jsx',
+    },
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js',
+    },
+});
+
+module.exports = main;
