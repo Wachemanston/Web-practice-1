bf93adfcf68377da9c30c1500bf75498f8281127
Arrmen Lee <wachemanstonphoenix@gmail.com>
2020-08-19 15:52:11 +0800

    (Webpack)[Modify] Resolve .jsx files & modulize CSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,63 @@ const path = require('path');
 const distPath = path.resolve(__dirname, 'dist');
 
 const config = {
-    resolve: {
-        extensions: ['.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/],
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                [
-                                    '@babel/env', {
-                                        modules: false,
-                                    },
-                                ],
-                            ],
-                            plugins: [
-                                'transform-object-rest-spread',
-                                '@babel/plugin-transform-react-jsx'
-                            ],
-                        },
-                    },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/env', {
+                    modules: false,
+                  },
                 ],
+              ],
+              plugins: [
+                'transform-object-rest-spread',
+                '@babel/plugin-transform-react-jsx',
+              ],
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    }
-                ]
-            }
+          },
         ],
-    },
-    devServer: {
-        contentBase: distPath,
-        compress: true,
-        port: 8080
-    }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: distPath,
+    compress: true,
+    port: 8080,
+  },
 };
 
 const main = Object.assign({}, config, {
-    entry: {
-        index: './src/index.jsx',
-    },
-    output: {
-        path: distPath,
-        filename: '[name].bundle.js',
-    },
+  entry: {
+    index: './src/index.jsx',
+  },
+  output: {
+    path: distPath,
+    filename: '[name].bundle.js',
+  },
 });
 
 module.exports = main;
