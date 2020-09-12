419368d7a851bfba43835412d25c088ca25e5fd8
Arrmen Lee <wachemanstonphoenix@gmail.com>
2020-09-12 15:20:31 +0800

    (Component)[Add] First section

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styles from './styles.css';
 import Navbar from './Navbar';
 import Section from './unity/Section';
 import Slider from './Slider';
+import { H1 } from './Title';
+import DefaultButton from './unity/Button';
 
 const Main = () => {
   return (
@@ -9,6 +12,19 @@ const Main = () => {
       <Navbar />
       <Section>
         <Slider />
+      </Section>
+      <Section>
+        <H1 text="Lorem Title" />
+        <div className={styles['row-img-container']}>
+          <img className={styles['half-img']} src="https://images.unsplash.com/photo-1599793072640-d8365956dc31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80" alt="" />
+          <img className={styles['half-img']} src="https://images.unsplash.com/photo-1599734699867-92dd706c8c64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80" alt="" />
+        </div>
+        <p className={styles['half-paragraph']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eaque et exercitationem expedita, illum iusto </p>
+        <p className={styles['half-paragraph']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eaque et exercitationem expedita, illum iusto </p>
+        <div className={styles['row-btn-container']}>
+          <DefaultButton><span className={styles['empz-btn']}>Lorem 1</span></DefaultButton>
+          <DefaultButton><span className={styles['empz-btn']}>Lorem 2</span></DefaultButton>
+        </div>
       </Section>
     </div>
   );
