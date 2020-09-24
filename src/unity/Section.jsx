4de30c82db4bab7f726b54270044a0ec875868e8
Arrmen Lee <wachemanstonphoenix@gmail.com>
2020-09-24 20:03:01 +0800

    (Component)[Modify] Add separator at the bottom of section

--- src/unity/Section.jsx
+++ src/unity/Section.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import styles from './styles.css';
 
 const Section = ({ children }) => {
-  return <section className={styles.section}>{ children }</section>;
+  return (
+    <section className={styles.section}>
+      { children }
+      <div className={styles['section-separator']} />
+    </section>
+  );
 };
 
 Section.propTypes = {
