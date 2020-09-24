1e33f4bef9c55289aeec9faddf6cc4f3c756cacd
Arrmen Lee <wachemanstonphoenix@gmail.com>
2020-09-24 17:40:59 +0800

    (Component)[Add] Second section

--- src/Main.jsx
+++ src/Main.jsx
@@ -26,6 +26,20 @@ const Main = () => {
           <DefaultButton><span className={styles['empz-btn']}>Lorem 2</span></DefaultButton>
         </div>
       </Section>
+      <Section>
+        <div className={styles['row-container']}>
+          <img className={styles['half-img']} src="https://images.unsplash.com/photo-1599793072640-d8365956dc31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80" alt="" />
+          <div className={styles['half-paragraph']}>
+            <H1 text="Lorem Title" />
+            <p className={styles['full-paragraph']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eaque et exercitationem expedita, illum iusto </p>
+            <p className={styles['full-paragraph']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eaque et exercitationem expedita, illum iusto </p>
+            <div className={styles['row-btn-container']}>
+              <DefaultButton><span className={styles['empz-btn']}>Lorem 1</span></DefaultButton>
+              <DefaultButton><span className={styles['empz-btn']}>Lorem 2</span></DefaultButton>
+            </div>
+          </div>
+        </div>
+      </Section>
     </div>
   );
 };
